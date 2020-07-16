3636a8501e5a4b2d0c154d4e58f91a16b584fae0
Ali Komijani <akpa125@gmail.com>
2020-07-16 15:25:00 +0430

    Updates

---
+++ precache-manifest.abdbcb96c9a5b55aa5f0dc179581fa9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc4f42a1ac48cbb5fc56d0bbb8cded3",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "077d248a312b10c25e45",
+    "url": "/monsters/static/css/main.76922130.chunk.css"
+  },
+  {
+    "revision": "3198c0ec89ce4a869c77",
+    "url": "/monsters/static/js/2.fda7116e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.fda7116e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077d248a312b10c25e45",
+    "url": "/monsters/static/js/main.2fd4aec7.chunk.js"
+  },
+  {
+    "revision": "28c7cc27b21cfacfd62e",
+    "url": "/monsters/static/js/runtime-main.8d89be33.js"
+  }
+]);
